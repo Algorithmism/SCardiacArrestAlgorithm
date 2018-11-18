15f715380a5ecb8e2065e61e6c55bfb9cc99abdf
Mark Maroki <markmaroki1@gmail.com>
2018-11-18 16:50:52 -0500

    Data now interacts with RRmean function and plots average data

--- RRMeanAlgorithm.py
+++ RRMeanAlgorithm.py
@@ -1,46 +1,55 @@
 # generate related variables
-
+import sys
 from numpy import mean
 from numpy import std
 from numpy.random import randn
-from numpy.random import seed
+
 from matplotlib import pyplot
 
-# seed random number generator
-#seed(1)
-
-# prepare data
-#x = 20 * randn(1000) + 100
-# y = x + (10 * randn(1000) + 50)
-# # summarize
-# print('x: mean=%.3f stdv=%.3f' % (mean(x), std(x)))
-# print('y: mean=%.3f stdv=%.3f' % (mean(y), std(y)))
-# # plot
-# pyplot.scatter(x, y)
-# pyplot.show()
-
 def main():
-    a = [1,2,3]
-    print(rrmean(a,12,40))
+    with open ("test.txt", "r") as testFile:
+        count = 0
+        for line in testFile:
+            count = count + 1
+            print(rrmean(line))
+            #prepare data
+            x = count
+            y = rrmean(line)
+            #plot
+            pyplot.scatter(x,y)
+        pyplot.suptitle("Random Data Averages")
+        pyplot.show()
+
 
 # rrmean algorithm cal
-def rrmean(vals, N, time):
+def rrmean(line):
 
-    #param N is how many entries/data points
-    n = len(vals)
+    # remove end brackets (strings are immutable)
+    line = line.replace('[','')
+    line = line.replace(']', '')
 
-    #start sum at init 0
+    # create list of strings separated by comma
+    valListCopy = line.split(',')
+
+    # Convert string elements to double
+    valList = [float(i) for i in valListCopy]
+    # param N is how many entries/data points
+    n = len(valList)
+
+    # start sum at init 0
     totalSum = 0
 
-    #sum up the data point values
+    # sum up the data point values in the list of floats
     for i in range(n):
-        totalSum += vals[i]
+        totalSum += valList[i]
         i = i+1
-    totalSum = sum(vals)
+    # get the sum needed for the average variable below
+    totalSum = sum(valList)
 
-    #total sum divided by the length of the list of numbers
+    # total sum divided by the length of the list of numbers
     avg = totalSum/n
-    #return the average
+
+    # return the average
     return (avg)
 
 main()
