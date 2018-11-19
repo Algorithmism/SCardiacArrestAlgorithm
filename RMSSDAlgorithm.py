68922468f8f12a62e420c88deccfc12648eb43bc
Mark Maroki <markmaroki1@gmail.com>
2018-11-19 01:34:30 -0500

    File headers and source filled out

--- RMSSDAlgorithm.py
+++ RMSSDAlgorithm.py
@@ -1,2 +1,34 @@
+########################################################################################################################
+# This program will implement an algorithm to calculate the square root of the mean of differences between all \n
+# adjacent R-R intervals (Heart Rate Variability Criteria #1 (HRV).
+#
+# Author: Mark Maroki
+#
+########################################################################################################################
+
+from matplotlib import pyplot
 import RRMeanAlgorithm.py
 
+def RMSSD(line):
+
+    return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################################################################################################
+# Algorithm idea from:
+# Raka, Annmarie, et al. “Computational Algorithms Underlying the Time-Based Detection of Sudden Cardiac Arrest
+# via Electrocardiographic Markers.” Applied Sciences, vol. 7, no. 9, 2017, p. 954., doi:10.3390/app7090954.
+#
+########################################################################################################################
